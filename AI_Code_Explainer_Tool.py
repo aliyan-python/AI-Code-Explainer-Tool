dd50c5abbd16be3ca25fb5e96b8ad3f0e038b503
aliyan-python <kaliyaan63@gmail.com>
2026-05-02 23:01:25 +0500

    First Commit

---
+++ AI_Code_Explainer_Tool.py
@@ -0,0 +1,68 @@
+# AI Code Explainer Tool
+# Objective: A tool that takes code as input and explains each line in simple language.
+
+def explain_code(code):
+    lines = code.split("\n")
+    explanation = []
+
+    print("AI Code Explainer Started\n")
+
+    for i, line in enumerate(lines, start=1):
+        line_lower = line.strip().lower()
+
+        if "for" in line_lower and "range" in line_lower:
+            explanation.append(
+                f"Line {i}: This line starts a loop that runs multiple times.")
+
+        elif "print" in line_lower:
+            explanation.append(f"Line {i}: This prints output to the screen.")
+
+        elif "=" in line_lower:
+            explanation.append(
+                f"Line {i}: This assigns a value to a variable.")
+
+        elif "if" in line_lower:
+            explanation.append(f"Line {i}: This checks a condition.")
+
+        elif "def" in line_lower:
+            explanation.append(f"Line {i}: This defines a function.")
+
+        else:
+            explanation.append(f"Line {i}: This is a code statement.")
+
+    return explanation
+
+
+while True:
+    code_lines = []
+
+    print("Enter your code (type 'done' to finish):")
+
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+
+        if line.strip().lower() == "done":
+            break
+
+        code_lines.append(line)
+
+    if not code_lines:
+        print("No code entered\n")
+        continue
+
+    code = "\n".join(code_lines)
+
+    results = explain_code(code)
+
+    print("\nExplanation:")
+    for r in results:
+        print(r)
+
+    choice = input(
+        "\nType 'exit' to quit or press Enter to continue: ").lower()
+    if choice == "exit":
+        print("Thanks For Using")
+        break
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# AI Code Explainer Tool
+
+## Overview:
+AI Code Explainer Tool is a Python-based application that takes user-input code and explains each line,It helps users understand how code works step-by-step.
+
+## Features:
+- Explain code line-by-line.  
+- Supports basic Python concepts (loops, print, variables, conditions).  
+- Beginner-friendly explanations.  
+- Continuous interactive mode.  
+- Works offline.  
+
+## How to Run:
+1. Make sure Python is installed.
+2. Open the project folder.
+3. Run the file using:
+
+   python_AI_Code_Explainer_Tool.py
